9fd094aac56d7e6b4eedd38694413e8370030713
IBRAHIM HOSSAIN <38831749+ibrahoss@users.noreply.github.com>
2025-11-29 01:19:55 +0200

    Update the plugin URI

--- sentifyd-avatar.php
+++ sentifyd-avatar.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name:       Sentifyd Avatar
- * Plugin URI:        https://sentifyd.io
+ * Plugin URI:        https://github.com/Sentifyd/sentifyd-avatar-plugin
  * Description:       Easily deploy the Sentifyd avatar web component on your WordPress site.
  * Version:           1.1.0
  * Requires at least: 6.3
